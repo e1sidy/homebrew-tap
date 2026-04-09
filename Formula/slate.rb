de15ad10232e37a731609bc6fe5ce82b6b9940ba
Sanchit Jaiswal <44247525+e1sidy@users.noreply.github.com>
2026-04-09 23:35:32 +0530

    Update slate formula to v0.4.1

--- Formula/slate.rb
+++ Formula/slate.rb
@@ -4,28 +4,28 @@
 class Slate < Formula
   desc "Lightweight task management CLI backed by SQLite"
   homepage "https://github.com/e1sidy/slate"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/e1sidy/slate/releases/download/v0.4.0/slate_0.4.0_darwin_amd64.tar.gz"
-      sha256 "883bddc64011a0813c643663d894eaeb6e3d6f632a44e96983d9fa6b74f3d63c"
+      url "https://github.com/e1sidy/slate/releases/download/v0.4.1/slate_0.4.1_darwin_amd64.tar.gz"
+      sha256 "0bbd492d8703eb81b2c7584f31fc0aa0f68a6273ee8110fbb73a4751583b2bc3"
     end
     on_arm do
-      url "https://github.com/e1sidy/slate/releases/download/v0.4.0/slate_0.4.0_darwin_arm64.tar.gz"
-      sha256 "a3a4aaa0a884a5acb397eb46469b70cc8f45bcf19ee8c10c2899645e1de08817"
+      url "https://github.com/e1sidy/slate/releases/download/v0.4.1/slate_0.4.1_darwin_arm64.tar.gz"
+      sha256 "2f0ced44780338b6cdb709d078fd6b3410f3b5504155105cdda52e39f8a4a0f6"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/e1sidy/slate/releases/download/v0.4.0/slate_0.4.0_linux_amd64.tar.gz"
-      sha256 "7a0be1ee56692616557c0ac9b0e3fe252440b231933396bc01b732dcb10887d6"
+      url "https://github.com/e1sidy/slate/releases/download/v0.4.1/slate_0.4.1_linux_amd64.tar.gz"
+      sha256 "92cc9286a5ae6605d28e3635eb82b66d914c9f3cf320a332e31a7cb5652ea31e"
     end
     on_arm do
-      url "https://github.com/e1sidy/slate/releases/download/v0.4.0/slate_0.4.0_linux_arm64.tar.gz"
-      sha256 "a17e5350c12cc8d1021bed7bbb2bef00e21e08b22c16800d2d0b7df3f7805874"
+      url "https://github.com/e1sidy/slate/releases/download/v0.4.1/slate_0.4.1_linux_arm64.tar.gz"
+      sha256 "7a133862d603b1971a4db733c625aaa7d5ce2cb8d8b1a9c753f9da60b1f5a054"
     end
   end
 
